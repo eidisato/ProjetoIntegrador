20ca368ff7589f172c6e1ffe95572aa4e32bdd96
eidisato <viniciuseidi@outlook.com>
2021-11-04 11:43:01 -0300

    Task 3 e 4

---
+++ Task_4/BACKEND/sustentei/src/main/java/br/org/generation/sustentei/SustenteiApplication.java
@@ -0,0 +1,13 @@
+package br.org.generation.sustentei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SustenteiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SustenteiApplication.class, args);
+	}
+
+}
---
+++ Task_4/BACKEND/sustentei/src/test/java/br/org/generation/sustentei/SustenteiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.generation.sustentei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SustenteiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
